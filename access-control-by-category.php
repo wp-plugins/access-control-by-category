f32fe908b54aae8999940ab1bc89b4ade7fdddf8
narin1975 <narin1975@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-31 07:46:41 +0000

    edit ver
    git-svn-id: https://plugins.svn.wordpress.org/access-control-by-category/tags/1.1@720734 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- access-control-by-category.php
+++ access-control-by-category.php
@@ -4,6 +4,7 @@ Plugin Name: Access Control by Category
 Plugin URI: http://dekisugi.net/wpplugins/access-control-by-category
 Description:  Visitors must logged on to see the content of any posts under "Subscribed" category. This can be used as a simple membership system. Please<a href="http://wordpress.org/extend/plugins/access-control-by-category"> [rate] </a> this plugin.
 Author: Narin Olankijanan
+Version: 1.1
 Author URI: http://dekisugi.net/wpplugins
 License: GPLv2
 */
