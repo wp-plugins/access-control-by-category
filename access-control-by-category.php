e8001d31337712546caa6a4b45277a704aad5ff8
narin1975 <narin1975@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-25 02:37:24 +0000

    import 1.2
    git-svn-id: https://plugins.svn.wordpress.org/access-control-by-category/tags/1.2@844944 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ access-control-by-category.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Plugin Name: Access Control by Category
+Plugin URI: http://dekisugi.net/wpplugins/access-control-by-category
+Description:  Visitors must logged on to see the content of any posts under "Subscribed" category. Any texts within [subscribed] shortcode will also be restricted to logged-in users. This can be used as a simple membership system. Please<a href="http://wordpress.org/extend/plugins/access-control-by-category"> [rate] </a> this plugin.
+Author: Narin Olankijanan
+Version: 1.2
+Author URI: http://dekisugi.net/wpplugins
+License: GPLv2
+*/
+
+/* Copyright 2012 Narin Olankijanan (email: narin@dekisugi.net)
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this progam; if not, write to the Free Software Foundation, Inc. 51 Franklin St, Fifth floor, Boston MA 02110-1301 USA
+*/
+add_shortcode('subscribed', 'dk_shortcode');
+
+function dk_shortcode ($attr, $content) {
+
+  if (is_user_logged_in()) {
+      return $content;
+  } else {
+      return "[You must log in to view the remaining texts...]";
+  }
+}
+
+
+add_filter('the_content', 'dk_res_post');
+
+function dk_res_post($content){
+        global $post; 
+	$post_id = $post->ID;
+        $cats = wp_get_post_categories( $post_id );
+	$is_restricted = false;
+	$post_categories = wp_get_post_categories( $post_id );
+	
+	foreach($post_categories as $c){
+		$cat = get_category( $c );
+     	        if ($cat->name == "Subscribed") {
+                  $is_restricted = true;
+                  break;
+                }		
+        }
+	if ($is_restricted AND !is_user_logged_in()) {
+          return "[You must log in to see the full content of this post.]";
+     
+	} else {
+          return $content;
+        }
+}
+
+
+
+/* EOF */
---
+++ readme.txt
@@ -0,0 +1,52 @@
+=== Access Control by Category ===
+Contributors: narin1975 
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=PKW36VZRWZPCY
+Tags: access, control, category, member, membership, restricted, limit, permission, posts, users, subscribers, read, view, login, logon
+Requires at least: 3.0.1
+Tested up to: 3.8.1
+Stable tag: 1.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Control access to registered users only of certain posts by assigning the "Subscribed" category to those posts.
+
+In additions, any texts within [subscribed] shortcode either in posts or pages will also be seen only by registered users.
+
+== Description ==
+
+Normally, visitors of a wordpress blog can view all published posts, whether or not they are being logged on. You can use this plugin to implement an access control whereby assigning the "Subscribed" category to any posts you want to control access The plugin will automatically hides the content of those posts to the non-logged-on users. This can be use as a simple membership system. For more information, visit <a href="http://dekisugi.net/wpplugins/access-control-by-category">official</a> page.
+
+** if your wordpress blog have the Jetpack plugin installed, do not enable the email subscription feature. Jetpack bypasses the system and get the contents of all posts and send them to any subscribers regardless of any control access. **
+
+
+== Installation ==
+
+1. Upload `access-control-by-category.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Create a post category called "Subscribed" (without quotes). The spelling has to be exact.
+4. For any posts you want to limit access to logged-in users only, simple assign the "Subscribed" category to them.
+5. That's it.
+
+== Frequently Asked Questions ==
+
+Not Yet
+
+== Screenshots ==
+
+1. screenshot-1.jpg
+2. screenshot-2.jpg
+
+== Changelog ==
+
+= 1.2=
+* shortcode functionality
+
+= 1.1.1 =
+* Update Info
+
+= 0.0 =
+* Initial Release
+
+== Upgrade Notice ==
+
+None
